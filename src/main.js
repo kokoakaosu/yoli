3d507fa61864cdd346ef7ac2e0f7449832f8dbe7
youyi <miuyouyi@qq.com>
2020-06-17 11:59:02 +0800

    el-container

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI, { size: 'small', zIndex: 2000 })
+
+// 关闭是生产模式下给出的提示
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/HomeHead.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="homeHead">
+    <h1>HomeHead</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeHead',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
